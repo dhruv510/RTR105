ffe94f5bc29190e0d5ead05abb77e73de8249716
dhruv <dhruvsavani510@gmail.com>
2020-10-03 13:41:36 +0530

    20200310_13_41

---
+++ GCC/code1.c
@@ -0,0 +1,10 @@
+
+// version 1.0.0
+// author Dhruv Savani
+// first comment row
+// second comment row
+main     (      )        {      } // comment in end of line
+/*
+comment
+/paragraph
+*/
---
+++ GCC/helloworld.c
@@ -0,0 +1,15 @@
+
+#include <stdio.h>
+
+int
+//void
+ main()
+ {
+ // how we output a text in shell: echo "some text"
+ // yes in the other laungagues you could use such an action (function) print
+ printf( "hello world!\n");
+
+ return 0;
+ }
+
+
