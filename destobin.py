d4c7efb2e0351b352959cf3fb6d24535e1ef5977
dhruv510 <60730843+dhruv510@users.noreply.github.com>
2020-10-22 17:32:59 +0530

    20202210_17_32
    20202210_17_32

--- destobin.py
+++ destobin.py
@@ -1,9 +1,23 @@
-integer=input()
-integer=int(integer)
-def binary(n): 
-    if (n > 1):  
-        binary(n >> 1) 
-    print(n & 1, end = "") 
-  
+#include<stdio.h>
+void binary(int n)
+{
+    unsigned i;
+    for (i = 1 << 31; i > 0; i = i / 2)
+    {
+        if(n&i)
+        {
+            printf("1");
+        }
+        else
+        {
+            printf("0");
+        }
+    }
+}
 
-binary(integer)
+int main(void)
+{
+    int num;
+    scanf("%d",&num);
+    binary(num);
+}
